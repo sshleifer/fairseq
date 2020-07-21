b260ed3f816d4bca25bb774ac1f35448df2aedb1
Sam Shleifer <sshleifer@gmail.com>
2020-07-21 15:01:31 -0400

    pickle save

--- trim_embs.py
+++ trim_embs.py
@@ -14,7 +14,8 @@ def load_dict(langs: List[str], path: str) -> Dictionary:
     d.add_symbol("<mask>")
     return d
 DEFAULT_LANGS="ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN"
-
+from durbango import pickle_save
+from pathlib import Path
 def main() -> None:
     """Usage:
     python trim_mbart.py --pre-train-dir ./mbart.cc25 --ft-dict ./ft/dict.xt  --output ./ft/model.pt
@@ -47,7 +48,7 @@ def main() -> None:
         model[name] = ft_tensor
 
     torch.save(data, args.output)
-
+    pickle_save(mapping, Path(args.output).parent.joinpath('mapping.pkl'))
 
 if __name__ == "__main__":
     main()
